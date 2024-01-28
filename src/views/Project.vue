42f28fe246a4b9fa6bba975b6e64dbfdfc942492
Aron Schüler <aron.schueler@ambient.digital>
2024-01-28 13:22:05 +0100

    feat: add text if no links provided

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -104,6 +104,14 @@ onMounted(() => {
         </dd>
       </div>
 
+      <div v-else class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+        <dt class="text-sm font-medium leading-6 text-gray-900">Links</dt>
+
+        <dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+          Never made it to production it seems! <span class="ml-1">📉</span>
+        </dd>
+      </div>
+
       <!-- Tags Section -->
       <div
         v-if="project?.tags && project.tags.length > 0"
